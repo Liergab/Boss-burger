e4f20a0d16c23d34514fd2c61e0b14a4a7aa18e2
unknown <bryangabrielberja25@gmail.com>
2022-10-15 08:45:40 +0800

    Update PORT

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.set('view engine, ejs')
 
 
 //Pagseset kung saan folder nakalagay ang mga html codes
-app.set('views', path.join(__dirname,'frontEnd/views'))
+app.set('views', path.join(__dirname,'/frontEnd/views'))
 
 
 //Serve static files
 //pag seset kung saan nakalagay ang nga asset at style
-app.use('/static', express.static(path.join(__dirname,'frontEnd/static')))
+app.use('/static', express.static(path.join(__dirname,'/frontEnd/static')))
 
 // middle ware
 // para mabasa ni node yung ibabatong data gamit si body
